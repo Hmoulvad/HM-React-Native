198e7d0161e3e8b4917750f15fa0cae00aea5de9
Hmoulvad <Hmoulvad@hotmail.com>
2019-04-24 13:20:42 +0200

    Started styling navigation bar.

--- components/navigation/navigation.component.tsx
+++ components/navigation/navigation.component.tsx
@@ -1,5 +1,13 @@
 import * as React from "react";
-import { StyleSheet, View, Image, Text } from "react-native";
+import {
+    StyleSheet,
+    View,
+    Text,
+    Image,
+    ViewStyle,
+    ImageStyle,
+    TextStyle
+} from "react-native";
 
 interface INavigationProps {}
 
@@ -14,14 +22,42 @@ class Navigation extends React.PureComponent<
     public render() {
         return (
             <View style={styles.navigation}>
-                <Image source={} />
-                <Text>Hey</Text>
+                <Image
+                    style={styles.icon}
+                    source={require("../../assets/images/menu.svg")}
+                />
+                <Text style={styles.title}>IMPACT</Text>
             </View>
         );
     }
 }
 export default Navigation;
 
-const styles = StyleSheet.create({
-    navigation: {}
+interface IStyles {
+    navigation: ViewStyle;
+    icon: ImageStyle;
+    title: TextStyle;
+}
+
+const styles: IStyles = StyleSheet.create({
+    navigation: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingTop: 6,
+        paddingBottom: 6,
+        height: 50,
+        width: "100%",
+        backgroundColor: "#000"
+    },
+    icon: {
+        width: 20,
+        height: 20
+    },
+    title: {
+        color: "#fff",
+        fontSize: 22,
+        lineHeight: 30
+    }
 });
